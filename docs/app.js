23c27549ad39712f8f9bbe625c0d85fa98d874c4
Tumurkhuyag <tomor.enkh@gmail.com>
2023-02-24 10:45:10 +0800

    create & modify style css, javascript for lesson list

---
+++ docs/app.js
@@ -0,0 +1,23 @@
+var lessonDone = 51;
+
+var str = "";
+
+for (let index = 6; index <= lessonDone; index++) {
+  if (index < 10) {
+    str +=
+      "<li><a href='/Lesson 0" +
+      index +
+      "/index.html'>Lesson 0" +
+      index +
+      "</a></li>";
+  } else {
+    str +=
+      "<li><a href='/Lesson " +
+      index +
+      "/index.html'>Lesson " +
+      index +
+      "</a></li>";
+  }
+}
+
+document.getElementById("lessons").innerHTML = str;
