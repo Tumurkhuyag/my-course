2d5d127f69216497afb6ae43edf0cd71bc449dad
Tumurkhuyag <tomor.enkh@gmail.com>
2023-11-14 20:41:45 +0800

    Lesson 26-27 update

--- 26-lesson/app.js
+++ 26-lesson/app.js
@@ -1,74 +1,74 @@
-// Бодлого №1: sum нь 1-1000 хүртэлх тоонуудын нийлбэр
-var sum = 0;
+function sumAll() {
+  // Бодлого №1: sum нь 1-1000 хүртэлх тоонуудын нийлбэр
+  var sum = 0;
 
-// Бодлого №2: sumSeven нь 1-1000 хүртэлх тооны 7-д хуваагддаг тоонуудын нийлбэр
-var sumSeven = 0;
+  // Бодлого №2: sumSeven нь 1-1000 хүртэлх тооны 7-д хуваагддаг тоонуудын нийлбэр
+  var sumSeven = 0;
 
-// Бодлого №2: sumSeven нь 1-1000 хүртэлх анхдагч тоонуудын нийлбэр
-var sumUnique = 0;
+  // Бодлого №2: sumSeven нь 1-1000 хүртэлх анхдагч тоонуудын нийлбэр
+  var sumUnique = 0;
 
-var start = 1;
-var end = 1000;
-var subtract = 7;
+  var start = 1;
+  var end = prompt("Хэд хүртэл тооны нийлбэрийг олох вэ?");
+  // var end = 1000;
+  var subtract = 7;
 
-for (var i = start; i <= end; i++) {
-  sum = sum + i;
-  // console.log("i = " + i + " байхад sum = " + sum);
-  if (i % subtract === 0) {
-    sumSeven = sumSeven + i;
-  }
-  var x = 0;
-  for (var j = 1; j <= i; j++) {
-    if (i % j === 0) {
-      x++;
+  for (var i = start; i <= end; i++) {
+    sum = sum + i;
+    // console.log("i = " + i + " байхад sum = " + sum);
+    if (i % subtract === 0) {
+      sumSeven = sumSeven + i;
     }
-    if (x > 2) {
-      break;
+    var x = 0;
+    for (var j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        x++;
+      }
+      if (x > 2) {
+        break;
+      }
+      // Тухайн тоо нь өөрийнхөө хагас тоо хүртэл ердөө 1 тоонд хуваагдаж буйг шалгана.
+      else if (i < 2 && i % 2 === 0) {
+        break;
+      }
     }
-    // Тухайн тоо нь өөрийнхөө хагас тоо хүртэл ердөө 1 тоонд хуваагдаж буйг шалгана.
-    else if (i < 2 && i % 2 === 0) {
-      break;
+    if (x <= 2) {
+      sumUnique = sumUnique + i;
+      // console.log("i = " + i + " байхад sumUnique = " + sumUnique);
     }
   }
-  if (x <= 2) {
-    sumUnique = sumUnique + i;
-    // console.log("i = " + i + " байхад sumUnique = " + sumUnique);
-  }
-}
 
-// Хариу №1
-console.log(
-  start + " -с " + end + " хүртэлх тоонуудын нийлбэр нь " + sum + " байна!"
-);
+  // Хариу №1
+  document.getElementById("problemOne").innerHTML =
+    start + " -с " + end + " хүртэлх тоонуудын нийлбэр нь " + sum + " байна!";
 
-// Хариу №2
-if (sumSeven === 0) {
-  console.log(
-    start +
-      " -с " +
-      end +
-      " -ын хооронд " +
-      subtract +
-      " -д хуваагддаг тоо алга байна!"
-  );
-} else {
-  console.log(
-    start +
-      " -с " +
-      end +
-      " -ын хооронд " +
-      subtract +
-      " -д хуваагддаг тоонуудын нийлбэр нь " +
-      sumSeven +
-      " байна!"
-  );
-}
+  // Хариу №2
+  sumSeven === 0
+    ? (document.getElementById("problemTwo").innerHTML =
+        start +
+        " -с " +
+        end +
+        " -ын хооронд " +
+        subtract +
+        " -д хуваагддаг тоо алга байна!")
+    : (document.getElementById("problemTwo").innerHTML =
+        start +
+        " -с " +
+        end +
+        " -ын хооронд " +
+        subtract +
+        " -д хуваагддаг тоонуудын нийлбэр нь " +
+        sumSeven +
+        " байна!");
 
-// Хариу №3
-if (sumUnique === 0) {
-  console.log(start + " -с " + end + " -ын хооронд анхдагч тоо алга байна!");
-} else {
-  console.log(
-    start + " -с " + end + " хүртэлх анхдагч тоонуудын нийлбэр нь " + sumUnique
-  );
+  // Хариу №3
+  sumUnique === 0
+    ? (document.getElementById("problemThree").innerHTML =
+        start + " -с " + end + " -ын хооронд анхдагч тоо алга байна!")
+    : (document.getElementById("problemThree").innerHTML =
+        start +
+        " -с " +
+        end +
+        " хүртэлх анхдагч тоонуудын нийлбэр нь " +
+        sumUnique);
 }
--- 27-lesson/app.js
+++ 27-lesson/app.js
@@ -1,38 +1,55 @@
-// Массивт давталтаар санамсаргүй 10 тоо оруулна
-var arr = [];
-for (var i = 0; i < 10; i++) {
-  var num = Math.random() * 100;
-  roundNum = Math.floor(num);
-  arr.push(roundNum);
-}
-
-console.log(arr);
-
-// Массивт байгаа бүх элементүүдийг шалгаж хамгийн бага утгыг MIN, хамгийн их утгыг MAX гэж хадгална
-var max = arr[0];
-var min = arr[0];
-
-for (var i = 1; i < arr.length; i++) {
-  if (arr[i] > max) {
-    max = arr[i];
-    var maxIndex = i;
+function createMassive() {
+  // Массивт давталтаар санамсаргүй 10 тоо оруулна
+  var promptValue = prompt(
+    "Хичнээн ширхэг санамсаргүй тоон өгөгдөлтэй массив үүсгэх вэ?"
+  );
+  var arr = [];
+  for (var i = 0; i < promptValue; i++) {
+    var num = Math.random() * 100;
+    roundNum = Math.floor(num);
+    arr.push(roundNum);
   }
 
-  if (arr[i] < min) {
-    min = arr[i];
-    var minIndex = i;
+  console.log(arr);
+  document.getElementById("problemOne").innerHTML =
+    arr +
+    " санамсаргүй утгууд бүхий " +
+    promptValue +
+    " ширхэг өгөгдөлтэй массив үүсгэлээ";
+
+  // Массивт байгаа бүх элементүүдийг шалгаж хамгийн бага утгыг MIN, хамгийн их утгыг MAX гэж хадгална
+  var max = arr[0];
+  var min = arr[0];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      var maxIndex = i;
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+      var minIndex = i;
+    }
   }
+
+  console.log("MAX = " + max + " , maxIndex =" + maxIndex);
+  console.log("MIN = " + min + " , maxIndex =" + minIndex);
+
+  document.getElementById("problemTwo").innerHTML =
+    "MAX = " + max + " , maxIndex =" + maxIndex;
+
+  document.getElementById("problemThree").innerHTML =
+    " MIN = " + min + " , minIndex =" + minIndex;
+
+  // MIN болон MAX тэнцүү биш бол энэ хоёрын байрыг солино
+  if (max !== min) {
+    var conNum = arr[maxIndex];
+    arr[maxIndex] = arr[minIndex];
+    arr[minIndex] = conNum;
+  }
+
+  // Сольсон массиваа хэвлэж үзүүлнэ
+  document.getElementById("problemFour").innerHTML =
+    arr + " Тоонууд солигдсон массив";
 }
-
-console.log("MAX = " + max, maxIndex);
-console.log("MIN = " + min, minIndex);
-
-// MIN болон MAX тэнцүү биш бол энэ хоёрын байрыг солино
-if (max !== min) {
-  var conNum = arr[maxIndex];
-  arr[maxIndex] = arr[minIndex];
-  arr[minIndex] = conNum;
-}
-
-// Сольсон массиваа хэвлэж үзүүлнэ
-console.log(arr);
