31b6bcccae931f164a764a539f26dd6a9ffbb426
Tumurkhuyag <tomor.enkh@gmail.com>
2023-12-01 14:31:49 +0800

    64-lesson, p2-project-bookkeeping update

--- p2-project-bookkeeping/app.js
+++ p2-project-bookkeeping/app.js
@@ -1,5 +1,25 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
 })();
 
 // Санхүүтэй ажиллах контроллер
@@ -7,24 +27,23 @@ var financeController = (function () {})();
 
 // Холбогч контроллер
 var appController = (function (uiController, appController) {
+  var DOM = uiController.getDOMstrings();
 
-  // Гүйлгээний мэдээллийг нэмж оруулах функц
-  var ctrlAddItem = function(){
-    console.log("Мэдээллийг татлаа")
-    // 1.Оруулах өгөгдлийг дэлгэцээс олж авна  
+  var ctrlAddItem = function () {
+    // 1.Оруулах өгөгдлийг дэлгэцээс олж авна
+    console.log(uiController.getInput());
     // 2.Оруулж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж, тэндээ хадгална
     // 3.Олж авсан өгөгдлүүдээ вебийнхээ тохирох хэсэгт нь гаргана
     // 4.Төсвийг тооцоолно
     // 5.Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана
-  }
+  };
 
-  document.querySelector('.add__btn').addEventListener('click', function(){
-    ctrlAddItem()
-  })
-  document.addEventListener('keypress', function(event){
-    if (event.key === 'Enter' || event.switch === "13") {
-      ctrlAddItem()
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
+    ctrlAddItem();
+  });
+  document.addEventListener("keypress", function (event) {
+    if (event.key === "Enter" || event.switch === "13") {
+      ctrlAddItem();
     }
-  })
-
+  });
 })(uiController, appController);
