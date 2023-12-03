983c34808b2cedb7340816d0fd38e6bf216d60a5
Tumurkhuyag <tomor.enkh@gmail.com>
2023-12-03 22:40:05 +0800

    67-lesson update

--- 67-lesson/app.js
+++ 67-lesson/app.js
@@ -1,25 +1,80 @@
-// financeController IIFE функц дотор public service бичиж далдлагдсан хувьсагч, функцуудад хандах
+var uiController = (function () {
+  var getInput = function () {
+    var type = document.getElementById("add__type").value;
+    var desc = document.getElementById("add__description").value;
+    var val = document.getElementById("add__value").value;
+    var input = { type, desc, val };
+    return input;
+  };
 
-// appController -оос financeController -ын private хувьсагч, функцуудруу хандах боломжтой болгох
+  // Далдлагдсан функц болон өгөгдлүүдрүү хандах боломжтой public service загварчлах
+  return {
+    transferInput: function () {
+      return getInput();
+    },
+  };
+})();
 
-// Массивын хамгийн сүүлийн элементийн утга дээр нэгийг нэмэх байдлаар гүйлгээний ID -ыг загварчлах
+uiController.transferInput();
 
-var data = {
-  items: {
-    inc: [],
-    exp: [],
-  },
+var myData = (function () {
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
 
-  totals: {
-    inc: 1000,
-    exp: 0,
-  },
-};
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
 
-var id = "id";
-var desc = "Цалин";
-var value = 250000;
+  // Далдлагдсан функц болон өгөгдлүүдрүү хандах боломжтой public service загварчлах
+  return {
+    publicService: function (type, desc, val) {
+      var Income = function (id, desc, val) {
+        this.id = id;
+        this.desc = desc;
+        this.val = val;
+      };
 
-data.items["inc"].push(desc);
-data.items["inc"].push(value);
-console.log(data.items["inc"]);
+      var Expense = function (id, desc, val) {
+        this.id = id;
+        this.desc = desc;
+        this.val = val;
+      };
+
+      var id;
+      // Обьектийн өгөгдлүүдрүү массивын хаалтаар хандах
+      data.items[type].length === 0
+        ? (id = 1)
+        : (id = data.items[type][data.items[type].length - 1].id + 1);
+
+      type === "inc"
+        ? data.items[type].push(new Income(id, desc, val))
+        : data.items[type].push(new Expense(id, desc, val));
+
+      return data.items[type];
+    },
+  };
+})();
+
+document.getElementById("btn__add").addEventListener("click", function () {
+  var input = uiController.transferInput();
+  var item = myData.publicService(input.type, input.desc, input.val);
+
+  console.log("item ", item);
+
+  // Массивын хамгийн сүүлийн элементийг олох динамик код бичих
+  var listObject = item[item.length - 1];
+
+  document.getElementById("show__last").innerHTML =
+    "<li>" +
+    listObject.id +
+    "  " +
+    listObject.desc +
+    "  " +
+    listObject.val +
+    "</li>";
+});
