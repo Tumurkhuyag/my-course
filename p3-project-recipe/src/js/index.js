9a8dac2de1d57b6f03a4f730864997b6cd4b8c9c
Tumurkhuyag <tomor.enkh@gmail.com>
2023-12-10 19:07:07 +0800

    116-lesson, p3-project-recipe modified

--- p3-project-recipe/src/js/index.js
+++ p3-project-recipe/src/js/index.js
@@ -1,6 +1,30 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+// Веб аппын төлөв
+// - Хайлтын query, үр дүн
+// - Тухайн үзүүлж байгаа жор
+// - Тухайн үзүүлж байгаа жор
+// - Like хийсэн жорууд
+// - Захиалж байгаа жорын найрлага
 
-search.doSearch().then((r) => console.log("result: ", r));
+const state = {};
+
+const controlSearch = () => {
+  console.log("Дарагдлаа");
+  // 1.Вебээс хайлтын түлхүүр үгийг гаргаж авна
+  const query = "pizza";
+
+  if (query) {
+    // 2.Тухайн түлхүүр үгээр хайдаг шинэ хайлтын обьектийг үүсгэж өгнө
+    // 3.Үүсгэсэн обьектоо state -рүүгээ хийнэ
+    // 4.Хайлт хийхэд зориулж дэлгэцийн UI бэлтгэнэ
+    // 5.Хайлтыг гүйцэтгэнэ
+    // 6.Хайлтын үр дүнг дэлгэцэнд үзүүлнэ
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
