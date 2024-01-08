d58400ea311fca28d8533ac9989dec88729ede5e
Tumurkhuyag <tomor.enkh@gmail.com>
2024-01-08 20:50:49 +0800

    /js-course

--- js-course/app.js
+++ js-course/app.js
@@ -5,14 +5,14 @@ var str = "";
 for (let index = 1; index <= lessonDone; index++) {
   if (index < 10) {
     str +=
-      "<li><a href='../0" +
+      "<li><a href='./0" +
       index +
       "-lesson/index.html'>Lesson 0" +
       index +
       "</a></li>";
   } else {
     str +=
-      "<li><a href='../" +
+      "<li><a href='./" +
       index +
       "-lesson/index.html'>Lesson " +
       index +
